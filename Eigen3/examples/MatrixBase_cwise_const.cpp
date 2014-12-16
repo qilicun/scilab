675d7dd4588645d8ea6ad7d8adf11617f0239e1d
Liu Ming <miliu@statoil.com>
2014-12-16 14:10:53 +0800

    compare coffecient-wise product and matrix product

--- Eigen3/examples/MatrixBase_cwise_const.cpp
+++ Eigen3/examples/MatrixBase_cwise_const.cpp
@@ -7,12 +7,14 @@ using namespace std;
 
 int main()
 {
-  Matrix3i m = Matrix3i::Random();
+  Matrix3f m = Matrix3f::Random();
   cout << "Here is the matrix m:" << endl << m << endl;
-  Matrix3i n = Matrix3i::Random();
+  Matrix3f n = Matrix3f::Random();
   cout << "And here is the matrix n:" << endl << n << endl;
   cout << "The coefficient-wise product of m and n is:" << endl;
   cout << m.cwise() * n << endl;
+  cout << "The product of m and n is:" << endl;
+  cout << m * n << endl;
   cout << "Taking the cube of the coefficients of m yields:" << endl;
   cout << m.cwise().pow(3) << endl;
 }
